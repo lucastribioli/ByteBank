2d971ca5c91dc8ff75f5dcc4f1992c6c94b6c8d0
lucastribioli <luk.tribioli.nunes@gmail.com>
2023-04-21 12:22:00 -0300

    some tests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,13 @@
 fun main(args: Array<String>) {
-    print("Hello World!")
-    val titular = "Lucas"
+    println("Hello World!")
+    val titular: String = "Lucas"
+    val numeroConta: Int = 999
+    var saldo: Double = 10.0
 
+    saldo += 100 + 2.0
+    saldo += 10
 
+    println("titular $titular")
+    println("Número da conta $numeroConta")
+    println("Saldo da conta $saldo")
 }
