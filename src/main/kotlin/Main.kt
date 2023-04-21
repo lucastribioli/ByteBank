1f6906d94bcf4c78df84ed116c8bf41d0a536e1f
lucastribioli <luk.tribioli.nunes@gmail.com>
2023-04-21 13:26:46 -0300

    study with 'when'

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ fun main(args: Array<String>) {
     println("Hello World!")
     val titular: String = "Lucas"
     val numeroConta: Int = 999
-    var saldo: Double = 10.0
+    var saldo: Double = 0.0
 
     saldo += 100 + 2.0
     saldo += 10
@@ -10,4 +10,16 @@ fun main(args: Array<String>) {
     println("titular $titular")
     println("Número da conta $numeroConta")
     println("Saldo da conta $saldo")
+
+    when {
+        saldo > 0 -> {
+            println("Tem dinheiro na conta")
+        }
+        saldo < 0 -> {
+            println("Sua conta está negativa")
+        }
+        else -> {
+            println("Não tem dinheiro na conta")
+        }
+    }
 }
