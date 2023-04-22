8e1015b9b2559fd1a2014f40a696fe5eb025ddc9
lucastribioli <luk.tribioli.nunes@gmail.com>
2023-04-22 10:29:42 -0300

    tests with codition for

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,24 @@
 fun main(args: Array<String>) {
     println("Hello World!")
-    val titular: String = "Lucas"
-    val numeroConta: Int = 999
-    var saldo: Double = 0.0
 
-    saldo += 100 + 2.0
-    saldo += 10
+    for (i in 1..5){
+        val titular: String = "Lucas $i"
+        val numeroConta: Int = 999 + i
+        var saldo: Double = 0.0
 
-    println("titular $titular")
-    println("Número da conta $numeroConta")
-    println("Saldo da conta $saldo")
+        saldo += 100 + 2.0 * i
+        saldo += 10
 
+        println("titular $titular")
+        println("Número da conta $numeroConta")
+        println("Saldo da conta $saldo")
+        testarCondicoes(saldo)
+
+    }
+
+
+}
+fun testarCondicoes(saldo: Double){
     when {
         saldo > 0 -> println("Tem dinheiro na conta")
         saldo < 0 ->println("Sua conta está negativa")
