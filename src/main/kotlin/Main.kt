205aaec60d6e8f5fbc44a6b986d3e3b16c0b02f1
lucastribioli <luk.tribioli.nunes@gmail.com>
2023-04-22 11:21:30 -0300

    added method in Conta class 'sacar'

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 fun main(args: Array<String>) {
     println("Bem vindo ao ByteBank")
     var conta = criarConta("Adilson", 12, 419.0)
+    conta.depositar(7666.0)
+    conta.sacar(10.0)
 
     println("Titular ${conta.titular}")
     println("Número da conta ${conta.numeroConta}")
@@ -8,6 +10,7 @@ fun main(args: Array<String>) {
 
     conta = criarConta("Joãozinho", 1343, 41553.0)
     conta.depositar(10.0)
+    conta.sacar(100000.0)
 
     println("Titular ${conta.titular}")
     println("Número da conta ${conta.numeroConta}")
@@ -22,6 +25,14 @@ class Conta {
     fun depositar(valor: Double){
         this.saldo += valor
         println("Valor depositado na conta do ${this.titular}, no valor de $valor")
+    }
+    fun sacar(valor:Double){
+        if (this.saldo >= valor){
+            this.saldo -= valor
+            println("Saque concluído com sucesso, seu saldo agora é ${this.saldo}")
+        }else{
+            println("Valor acima do permitido para saque, você tem apenas ${this.saldo}")
+        }
     }
 }
 
